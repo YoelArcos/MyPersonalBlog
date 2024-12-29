73225063e2b5e77015acabb009d3f7c5aa62f4de
YoelArcos <Yoel.arcoslopez@hotmail.com>
2024-12-29 13:33:11 +0100

    Fixes missing json translation.

--- MyBlog/src/app/app.module.ts
+++ MyBlog/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
@@ -10,18 +13,29 @@ import { CursorComponent } from './cursor/cursor.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { ContactComponent } from './contact/contact.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CursorComponent,
     AboutmeComponent,
-    ContactComponent
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MyBlog/src/app/home/home.component.ts
+++ MyBlog/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
 
-}
+  safeContent: SafeHtml | null = null;
+
+  constructor(private translate: TranslateService, private sanitizer: DomSanitizer) {
+    this.translate.setDefaultLang('en');
+
+    this.translate.get('about.paragraph3').subscribe((content: string) => {
+      this.safeContent = this.sanitizer.bypassSecurityTrustHtml(content);
+    });
+  }
+    switchLanguage(language: string) {
+      this.translate.use(language);
+
+      this.translate.get('about.paragraph3').subscribe((content: string) => {
+        this.safeContent = this.sanitizer.bypassSecurityTrustHtml(content);
+      });
+    }
+  }
+
